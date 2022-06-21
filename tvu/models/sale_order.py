c3c7ab1305e47bbcff818c1d959c91b6d2039412
sast-odoo <sast@odoo.com>
2022-06-21 11:46:08 -0400

    [ADD] started the expiration cron method

--- tvu/models/sale_order.py
+++ tvu/models/sale_order.py
@@ -2,8 +2,16 @@
 
 from odoo import api, fields, models
 from datetime import datetime
+from datetime import timedelta
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     expiration = fields.Datetime(string="Expiration")
+
+# the auto-update cron job to run at midnight every day
+    def _cancel_expired_orders(self):
+        for record in self:
+            if self.expiration: #if expiration is set; otherwise ignore
+                if datetime.now().day > self.expiration.day: #if this day is past the expiration day
+                    pass
