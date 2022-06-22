5a7f6f61ea0a85be1cb1c941692beaf2014d674b
sast-odoo <sast@odoo.com>
2022-06-22 10:55:25 -0400

    [ADD] Basic module, with a view to add fields 'pair per case' and 'price to pair' to tree view of product_template model.

---
+++ pw_shoes/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    "name" : "P&W Shoes",
+
+    "depends" : ["product"],
+
+    "data" : [
+        "views/product_views_inherit.xml"
+    ],
+
+    "demo" : []
+
+
+}
